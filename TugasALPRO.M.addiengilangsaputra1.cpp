7faf1bc281147038076eb491338b81c2d206ad66
AddienGilang <114645629+AddienGilang@users.noreply.github.com>
2022-10-09 17:36:29 +0700

    Add files via upload

---
+++ TugasALPRO.M.addiengilangsaputra1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main(){
+   int umur = 18;
+   string npm = "2210631170027";
+   string nama = "M.Addien Gilang Saputra";
+   string kelas = "1D";
+   string hobi = "Play Game,Olahraga";
+   string cita = "Jago Coding dan bekerja di start up ternama";
+   cout << "NPM: " << npm << endl;
+   cout << "Nama Lengkap: " << nama << endl;
+   cout << "Umur: " << umur << endl;
+   cout << "Kelas: " << kelas << endl;
+   cout << "Hobi: " << hobi << endl;
+   cout << "Cita-cita: " << cita << endl;
+
+   return 0;
+}
